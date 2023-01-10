d4ba0a6a0ec62db98b0cf1a87c6464f403b58036
rahulwagh <rahul.wagh@jhooq.com>
2023-01-10 23:20:26 +0200

    Terraform virtual machine on google cloud

--- virtual-machine/provider.tf
+++ virtual-machine/provider.tf
@@ -6,26 +6,3 @@ terraform {
     }
   }
 }
-
-provider "google" {
-  credentials = file("/Users/rahulwagh/.gcp/credentials.json")
-  project     = var.project_id
-  region  = "europe-west4"
-  zone    = "europe-west4-a"
-}
-
-resource "google_compute_instance" "terraform-managed-e2-small" {
-  name         = "terraform"
-  machine_type = "e2-small"
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    network = "default"
-    access_config {
-    }
-  }
-}
